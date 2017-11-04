7987e056f17b8a392260d936e696b67e33327e63
sakshaat_dc <sakshaat_dc@hotmail.com>
2017-11-04 02:51:45 -0400

    fixing port

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const port = process.env.port || 8080
 
 app.use(express.static('static'));
 
 app.get('/',
     (req, res) => res.sendFile(path.join(__dirname + "/index.html")));
 
-app.listen(3000, () =>
-    console.log('Example app listening on port 3000'));
+app.listen(port, () =>
+    console.log('Example app listening on port ' + port));
