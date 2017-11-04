c2201d907b93b56a703ecbfe553dd3c7516a370e
sakshaat_dc <sakshaat_dc@hotmail.com>
2017-11-04 04:39:33 -0400

    Added hot reload

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,28 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var cssnano = require('gulp-cssnano');
 
-gulp.task("default", () => {
+var browserSync = require('browser-sync').create();
+
+// Server for browser sync
+gulp.task('browser-sync', function() {
+    browserSync.init(['*.html', "build/css/*.css"], {
+        server: ['./', 'build']
+    });
+});
+
+/* Reload task */
+gulp.task('bs-reload', function() {
+    browserSync.reload();
+});
+
+// Gulp task which watches for changes in html and css file
+gulp.task("watch", ['browser-sync'], () => {
+    gulp.watch(["*.html"], ["bs-reload"]);
+    gulp.watch(["static/css/*.css"], ["build", "bs-reload"]);
+})
+
+// Build the css and js files
+gulp.task("build", () => {
     gulp.src("static/css/*.css")
         .pipe(concat("style.css"))
         .pipe(cssnano())
